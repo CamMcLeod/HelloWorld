69574978126a1dd9a868fcacd071b3251be704dd
Cam McLeod <cammcleod3@gmail.com>
2019-05-13 20:37:37 -0700

    initial commmit of completed assignment 1

---
+++ Hello World/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Cameron Mcleod on 2019-04-22.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    // remember to end command lines with a semi-colon!
+    printf("Cam likes to ski tour!\n");
+    printf("Cam grew up in 6 different countries!\n");
+    printf("Cam has a degree in Engineering Physics from UBC!\n");
+    return 0;
+}
